0f2c47004e77b0cade9c6098bae279f9ff2ef396
Martin Iglic <martin.iglic@gmail.com>
2022-02-19 11:24:56 +0100

    dodal sem podatke o vremenu

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,5 +1,14 @@
 # 2. faza: Uvoz podatkov
 
+require(dplyr)
+require(tidyr)
+require(readr)
+require(readxl)
 
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
+vremenskipodatki <- read_csv2("podatki/vremenski_podatki_za_analizo_pridelave_hrane/prenos",
+                              col_names=c("st_postaje","ime_postaje","obdobje","","poveprecna_temperatura", "maksimalna_temperatura"),
+                              skip=3, na="-",
+                              locale=locale(encoding="Windows-1250")))
+                             
